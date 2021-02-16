9e7e81201992e94e1747bc94adc7e5060f9807e3
Daemonium97 <daemonium97@gmail.com>
2021-02-16 22:40:45 +0000

    ST2

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,9 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<img src="https://img.icons8.com/color/64/000000/star-wars.png" />
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,25 +1,34 @@
 import React from "react";
-import { UncontrolledCarousel } from "reactstrap";
-import PropTypes from "prop-types";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
 
-const items = [
-	{
-		src: "https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2016/09/10StarkillerBase.jpeg",
-		header: "Characters",
-		key: "1"
-	},
-	{
-		src: "https://pyxis.nymag.com/v1/imgs/043/29d/3cd1f3bd82677f9456b4209407822e487d-bespin.2x.w710.jpg",
-		header: "Planets",
-		key: "2"
-	}
-];
+export const Carrusel = () => {
+	return (
+		<Carousel>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src="https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2016/09/10StarkillerBase.jpeg"
+					alt="First slide"
+				/>
+				<Carousel.Caption>
+					<h3>Characters</h3>
+					<p>Jedi, Sith, Rebels</p>
+				</Carousel.Caption>
+				<Link to="/people" />
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src="https://pyxis.nymag.com/v1/imgs/043/29d/3cd1f3bd82677f9456b4209407822e487d-bespin.2x.w710.jpg"
+					alt="Second slide"
+				/>
 
-export const Carrusel = () => <UncontrolledCarousel items={items} />;
-
-UncontrolledCarousel.propTypes = {
-	items: PropTypes.array.isRequired,
-	indicators: PropTypes.bool, // default: true
-	controls: PropTypes.bool, // default: true
-	autoPlay: PropTypes.bool // default: true
+				<Carousel.Caption>
+					<h3>Planets</h3>
+					<p>Where are you from?</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
+	);
 };
